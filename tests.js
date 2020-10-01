95743e6502c8d82b92fde6dd0402ac195d40552b
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-01 15:46:30 -0500

    adjusting SayHello('Jane') portion of unit testing

--- tests.js
+++ tests.js
@@ -24,13 +24,16 @@ describe('sayHello', function () {
         expect(typeof sayHello()).toBe("string");
     });
     it('should return the string "Hello, Jane!" when executed', function() {
-        expect(sayHello()).toBe("Hello, Jane!");
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
     it('should return the string "Hello, Alex!" when executed', function() {
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
     it('should return the string "Hello, Pat!" when executed', function () {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(sayHello()).toBe("Hello, World!");
     });
 });
 
